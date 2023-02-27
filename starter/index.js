ebe72a72ad18efdbd67612925f788fa28e250d9d
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-02-27 23:31:42 +0000

    readme generator works

--- starter/index.js
+++ starter/index.js
@@ -10,50 +10,90 @@ const generateMarkdown = require("./utils/generateMarkdown");
 inquirer.prompt([
     {
       type: 'input',
-      name: '#Title',
+      name: 'Title',
       message: 'What is the title of your project?',
     },
     {
       type: 'input',
-      name: '#Description',
-      message: 'Where would you like to say in your description?',
+      name: 'Description',
+      message: 'What would you like to say in your description?',
     },
     {
       type: 'input',
-      name: '#Contents',
-      message: 'What would you like to have in your Table of Contents?',
-    },
-    {
-      type: 'input',
-      name: '#Installation',
-      message: 'Are there any installation requirements?',
-    },
-    {
-      type: 'input',
-      name: '#Usage',
+      name: 'Usage',
       message: 'Please provide instructions for use',
     },
     {
       type: 'input',
-      name: '#Credits',
+      name: 'Credits',
       message: 'Please proide your list of collaborators if any',
     },
     {
         type: 'input',
-        name: '#Tests',
+        name: 'Tests',
         message: 'Please provide examples on how to run your tests',
       },
       {
         type: 'input',
-        name: '#Questions',
+        name: 'Questions',
         message: 'Please enter your github username and link',
       },
+      {
+        type: 'input',
+        name: 'License',
+        message: 'Which License would you like to add to this project?',
+      },
      
-  ])
+     ])
+ 
+    
 
-  .then((response) => {
-    console.log(response)
-    fs.writeFile(`ReadMe.md`,JSON.stringify(response), (error)=>{
+
+
+
+.then ((response) => {
+    return fs.writeFile(`ReadMe.md`, `
+  #${response.Title}
+
+  # Description
+
+  ${response.Description}
+
+
+  ## Table of Contents
+
+  -Description (#Description)
+  -Installation (#Installation)
+  -Usage (#Usage)
+  -Contributions (#Contributions)
+  -Tests (#Tests)
+  -License (#License)
+
+  ## Installation
+
+  ${response.Installation}
+
+
+  ## Usage
+
+  ${response.usage}
+
+
+  ## Contributions
+
+  ${response.Credits}
+
+
+  ## Tests
+
+  ${response.Tests}
+
+
+  ## License
+
+  The license for this project is ${response.License}
+  `,
+    (error)=>{
         return error
         ? CSSConditionRule.error 
         : console.log("Success")
